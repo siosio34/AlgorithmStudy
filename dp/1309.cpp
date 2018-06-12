9281ae3e2ab656641a4091bb81e800be54386c31
youngje <siosio34@nate.com>
2018-06-12 13:45:55 +0000

    dp : 1309.cpp - Solve

---
+++ dp/1309.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+long long dp[100001][3];
+
+int main() {
+	
+	dp[0][0] = 1;
+	int n;
+	cin >> n;
+	
+	for(int i = 1 ; i <= n ; i++) {
+		dp[i][0] = (dp[i-1][0] + dp[i-1][1] + dp[i-1][2]) % 9901;
+		dp[i][1] = (dp[i-1][0] + dp[i-1][2]) % 9901;
+		dp[i][2] = (dp[i-1][0] + dp[i-1][1]) % 9901;
+	}
+	cout << (dp[n][0] + dp[n][1] + dp[n][2]) % 9901;
+}
