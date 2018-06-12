3ee3b19676895d15a654104be30ff11076d9739a
youngje <siosio34@nate.com>
2018-06-12 13:45:55 +0000

    dp : 11727.cpp - Solve

---
+++ dp/11727.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+long long dp[1001];
+
+int main() {
+	int n;
+	cin >> n;
+	
+	dp[0] = 0;
+	dp[1] = 1;
+	dp[2] = 3;
+	
+	for(int i = 3 ; i <= n ; i++) {
+		dp[i] = (dp[i-1] + 2 * dp[i-2]) % 10007;
+	}
+	
+	cout << dp[n] % 10007;
+}
