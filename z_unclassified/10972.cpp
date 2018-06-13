3ab319026daa4cbd5c0ee947eecca1208bd28c51
youngje <siosio34@nate.com>
2018-06-13 08:10:13 +0000

    z_unclassified : 10972.cpp - Solve

---
+++ z_unclassified/10972.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+int a[10001];
+int b[10001];
+int main() {
+	int n;
+	cin >> n;
+	
+	for(int i = 0 ; i < n ; i++) {
+		scanf("%d",&a[i]);
+	}
+	int cnt = 0;
+	do {
+		if(cnt == 0) {
+			cnt++;
+			continue;
+		} else {
+			for(int i = 0 ; i < n ; i++) {
+				cout << a[i] << " ";
+			} 
+			
+			return 0;
+		}
+	} while (next_permutation(a, a+n));
+	
+	cout << -1 << endl;
+	return 0;
+}
