208a270f4724c12d3fbd832890870bda035723eb
youngje <siosio34@nate.com>
2018-06-13 08:10:13 +0000

    z_unclassified : 11054.cpp - Solve

---
+++ z_unclassified/11054.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int a[1001];
+int dp_left[1001];
+int dp_right[1001];
+
+int main() {
+    int num;
+    cin >> num;
+    
+    for(int k =1 ; k<= num ; k++) {
+        cin >> a[k];
+        dp_left[k] = 1;
+        dp_right[k] = 1;
+    }
+    
+    for(int i=1; i<= num ; i++) {
+        for(int j=1 ; j<i ; j++) {
+            if(a[j] < a[i] && dp_left[i] <= dp_left[j])
+                dp_left[i] = dp_left[j] + 1;
+        }
+    }
+    
+    for(int i=num; i >= 1 ; i--) {
+        for(int j=num ; j > i ; j--) {
+            if(a[j] < a[i] && dp_right[i] <= dp_right[j])
+                dp_right[i] = dp_right[j] + 1;
+        }
+    }
+    
+    int max = 0;
+    for(int i=1 ; i<=num ; i++) {
+        if(max < dp_left[i] + dp_right[i]) {
+            max = dp_left[i] + dp_right[i];
+        }
+    }
+    
+    cout << max -1 << endl;
+}
