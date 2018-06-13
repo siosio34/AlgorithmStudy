f067d6385b50ccc0272fb74830c3c7da57068aff
youngje <siosio34@nate.com>
2018-06-13 08:10:14 +0000

    z_unclassified : 2558.cpp - Solve

---
+++ z_unclassified/2558.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int sum =0;
+    for(int i = 0 ;i < 2 ; i++) {
+        int temp;
+        cin >> temp;
+        sum+= temp;
+    }
+    cout << sum << endl;
+    
+}
