8c2d91040dc629884a842036bbfcdfac1812fa19
youngje <siosio34@nate.com>
2018-06-13 08:10:13 +0000

    z_unclassified : 10950.cpp - Solve

---
+++ z_unclassified/10950.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+using namespace std;
+
+int main() {
+    int t;
+    int a, b;
+    scanf("%d", &t);
+    while (t--) {
+        scanf("%d %d", &a,&b);
+        printf("%d\n", a+b);
+    }
+}
