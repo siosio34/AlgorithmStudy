91384dc1a7e9a1ab2c9e8b9da9112a86fd8dd4e9
youngje <siosio34@nate.com>
2018-06-13 08:10:14 +0000

    z_unclassified : 2294-도전2.cpp - Solve

---
+++ z_unclassified/2294-도전2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int n, k;
+int vec[10001];
+int dp[10001];
+int ans = 0;
+
+int main() {
+
+	cin >> n >> k;
+
+	for (int i = 0; i < n; i++) {
+		cin >> vec[i];
+	}
+
+	for (int i = 0; i <= k; i++) {
+		dp[i] = -1;
+	}
+
+	dp[0] = 0;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j <= k; j++) {
+			if (j - vec[i] >= 0 && dp[j - vec[i]] != -1) {
+				if (dp[j] == -1 || dp[j] > dp[j - vec[i]] + 1) {
+					dp[j] = dp[j - vec[i]] + 1;
+				}
+			}
+		}
+	}
+	cout << dp[k];
+
+}
