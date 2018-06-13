5ca5061f4d706e39ba8f54c7e1dd8f6ecb49da74
youngje <siosio34@nate.com>
2018-06-13 08:10:14 +0000

    z_unclassified : 2747.cpp - Solve

---
+++ z_unclassified/2747.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+long long a[46];
+int main() {
+	a[0] = 0 ;
+	a[1] = 1;
+	
+	for(int i = 2 ; i <= 45 ; i++) {
+		a[i] = a[i-1] + a[i-2];
+	}
+	int n;
+	cin >> n;
+	
+	cout << a[n];
+}
