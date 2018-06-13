c792884097d719266dc389cf8c7801a5d4650206
youngje <siosio34@nate.com>
2018-06-13 08:10:14 +0000

    z_unclassified : 2480.cpp14.cpp - Solve

---
+++ z_unclassified/2480.cpp14.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+using namespace std;
+
+int main() {
+
+	
+	int a,b,c;
+
+		cin >> a >> b >> c;
+		
+		int ans;
+		if(a== b && b == c) {
+			ans = 10000 + a * 1000;
+		} else if(a == b || a == c) {
+			ans = 1000 + a * 100;
+		} else if(b == c) {
+			ans = 1000 + b * 100;
+		} else {
+			ans = max({ a,b,c }) * 100;
+		}
+	
+			
+	
+	cout << ans << endl;
+}
