ad4942e859575122f012498e4586aac7ec2d3c8d
youngje <siosio34@nate.com>
2018-06-13 08:10:15 +0000

    z_unclassified : 4999.cpp17.cpp - Solve

---
+++ z_unclassified/4999.cpp17.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string a,b;
+	cin >> a >> b;
+	
+	if(a.find(b) == string::npos) {
+		cout << "no";
+	} else {
+		cout << "go";
+	}
+	
+}
