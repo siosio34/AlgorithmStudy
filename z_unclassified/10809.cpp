69e2b5b453ecaac1b5074e54e84d58b007291d17
youngje <siosio34@nate.com>
2018-06-13 08:10:13 +0000

    z_unclassified : 10809.cpp - Solve

---
+++ z_unclassified/10809.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string s;
+	cin >> s;
+	
+	int arr[26];
+	
+	for(int i = 0 ; i < 26 ; i++) {
+		arr[i] = -1;
+	}
+	
+	for(int i = 0 ; i < s.length() ; i++) {
+		if(arr[int(s[i]) - 97] == -1)
+			arr[int(s[i]) - 97] = i; 
+	}
+	
+	for(int i = 0 ; i < 26 ; i++) { 
+		cout << arr[i] << " ";
+	}
+}
