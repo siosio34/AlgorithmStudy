d6b570b21eec3c200f29a4cc506c3d81955daa53
youngje <siosio34@nate.com>
2018-06-13 08:10:14 +0000

    z_unclassified : 1904.cpp - Solve

---
+++ z_unclassified/1904.cpp
@@ -0,0 +1,19 @@
+// #include <iostream>
+// using namespace std;
+
+// int dp[1000001];
+
+// int main() {
+// 	int n;
+// 	cin >> n;
+// 	dp[0] = 0;
+// 	dp[1] = 1;
+// 	dp[2] = 2;
+	
+// 	for(int i = 3 ; i<=n ;i++){
+// 		dp[i] = dp[i-1]+ dp[i-2];
+// 		dp[i] = dp[i] % 15746;
+// 	}
+// 	cout << dp[n]  << endl;
+	
+// }
