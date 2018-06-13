451cc5f67edcf88c6dec9ee03991731e222891c4
youngje <siosio34@nate.com>
2018-06-13 08:10:14 +0000

    z_unclassified : 2475.cpp - Solve

---
+++ z_unclassified/2475.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int sum=0;
+	int num;
+	
+
+	for(int i=0 ; i<5 ; i++)
+	{
+		cin>>num;
+		sum += (num*num);
+	}
+	cout<< sum%10;
+	return 0;
+}
