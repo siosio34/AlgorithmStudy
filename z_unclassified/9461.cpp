ec5d5efe2108a040be1b5f7d94e0fa050312ec2b
youngje <siosio34@nate.com>
2018-06-13 08:10:16 +0000

    z_unclassified : 9461.cpp - Solve

---
+++ z_unclassified/9461.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+long long dp[101] ={0};
+vector<long long> ans;
+
+void go(int i) {
+    if(dp[i] != 0)
+        return ;
+    
+    dp[i] = dp[i-1] + dp[i-5];
+}
+
+int main() {
+    int t;
+    cin >> t;
+    
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 1;
+    dp[3] = 1;
+    dp[4] = 2;
+    
+    while(t--) {
+        int num;
+        cin >> num;
+        
+        for(int i = 5 ; i<=num ; i++) {
+            go(i);
+        }
+        // cout << dp[num] << endl;
+         ans.push_back(dp[num]);
+    }
+    
+    for(int i = 0 ; i < ans.size() ; i++) {
+        cout << ans[i] << endl;
+    }
+}
