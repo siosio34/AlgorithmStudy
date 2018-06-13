519e8443a15c8c8f194eb92f3c6e76bad821ea08
youngje <siosio34@nate.com>
2018-06-13 08:10:13 +0000

    z_unclassified : 1242.cpp - Solve

---
+++ z_unclassified/1242.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N, M, K;
+vector<int> cache1;
+
+int main()
+{
+	cin >> N >> M >> K;
+	int count = 1;
+	int i = 0;
+
+
+	int position = K - 1;
+
+	for (int i = 0; i < N; i++)
+	{
+		cache1.push_back(i);
+	}
+
+	while (1)
+	{
+		i = (i + M - 1) % N;
+
+		if (i == position || N == 1)
+			break;
+
+		else
+		{
+			cache1[i] = move(cache1.back());
+			cache1.pop_back();
+
+			if (i < position)
+			{
+				position--;
+			}
+
+		}
+
+		N--;
+		count++;
+	}
+
+	cout << count;
+	return 0;
+
+}
