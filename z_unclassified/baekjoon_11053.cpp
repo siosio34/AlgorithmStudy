baddbb677daba4c8dc5f832aa47983a6695675af
youngje <siosio34@nate.com>
2018-06-13 08:10:16 +0000

    z_unclassified : baekjoon_11053.cpp - Solve

---
+++ z_unclassified/baekjoon_11053.cpp
@@ -0,0 +1,31 @@
+// #include <iostream>
+// using namespace std;
+
+// int dp[1001];
+// int a[1001];
+
+// int max_len = 1;
+// int main() {
+//     int num;
+//     cin >> num;
+	
+	
+    
+//     for(int i = 1 ; i<= num ; i ++) {
+//         cin >> a[i];
+// 		dp[i] = 1;
+//     }
+    
+//     for(int i =1 ; i<= num ; i++) {
+        
+//         for(int j = 1; j < i ; j++) {
+//             if(a[i] > a[j] && dp[j] >= dp[i]) {
+//                 dp[i] = dp[j] + 1;
+//                 if(max_len < dp[i])
+//                     max_len = dp[i];
+//             }
+//         }
+//     }
+    
+//     cout << max_len << endl;
+// }
