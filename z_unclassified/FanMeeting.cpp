db47a98a67906d7ed1b13f23209bbf58fd231635
youngje <siosio34@nate.com>
2018-06-13 08:10:16 +0000

    z_unclassified : FanMeeting.cpp - Solve

---
+++ z_unclassified/FanMeeting.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int num;
+string temp1, temp2;
+vector<string> singer;
+vector<string> fan;
+int hugNum;
+
+int CountHug()
+{
+	                                      
+}
+int main()
+{
+	cin >> num;
+
+	for (int i = 0; i < num; i++)
+	{
+		cin >> temp1 >> temp2;
+		singer.push_back(temp1);
+		fan.push_back(temp2);
+
+	}
+
+}
