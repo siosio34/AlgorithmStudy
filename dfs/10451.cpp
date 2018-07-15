ca542f47c3fc3fcf44ecefb96fb65c2131e6f829
youngje <siosio34@nate.com>
2018-07-15 14:46:32 +0000

    dfs : 10451.cpp - Solve

---
+++ dfs/10451.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+bool checked[1001];
+
+void dfs(int x) {
+
+}
+
+int main() {
+	
+	int t;
+	cin >> t;
+	
+	while(t--) {
+		memset(checked,false,sizeof(checked));
+		
+	}
+}
