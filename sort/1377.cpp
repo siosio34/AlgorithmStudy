14fc833027d9ac57456592ccf514c31db50ee761
youngje <siosio34@nate.com>
2018-07-15 14:46:33 +0000

    sort : 1377.cpp - Solve

---
+++ sort/1377.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	vector<pair<int,int> > a(n);
+	for(int i = 0 ; i < n ; i++) {
+		cin >> a[i].first;
+		a[i].second = i;
+	}
+	sort(a.begin(), a.end());
+	int ans = 0;
+	for(int i = 0 ; i < n ; i++) {
+		if(ans < a[i].second - i) {
+			ans = a[i].second - i;
+		}
+	}
+	cout << ans + 1;
+}
