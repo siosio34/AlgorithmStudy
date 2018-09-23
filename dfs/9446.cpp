37a1167663bd79d39d4d4e6826f610d57850888a
youngje <siosio34@nate.com>
2018-09-23 11:10:14 +0000

    dfs : 9446.cpp - Solve

---
+++ dfs/9446.cpp
@@ -0,0 +1,15 @@
+// 사이클갯수 찾기, 언제 어떤 dfs 에서 찾았는지필요.
+
+int dx[4] = {0,0,1,-1};
+int dy[4] = {1,-1,0,0};
+
+#include <iostream>
+using namespace std;
+
+void bfs(int x,int y, int cnt) {
+	
+}
+
+int main() {
+	int cnt = 0;
+}
