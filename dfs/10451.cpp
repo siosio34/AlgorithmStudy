2b1f599776cd373b117618495ad9395778b0ad26
youngje <siosio34@nate.com>
2018-09-23 11:10:13 +0000

    dfs : 10451.cpp - Solve

--- dfs/10451.cpp
+++ dfs/10451.cpp
@@ -3,9 +3,12 @@
 using namespace std;
 
 bool checked[1001];
+int a[1001];
 
 void dfs(int x) {
-
+	if(checked[x]) return ;
+	checked[x] = true;
+	dfs(a[x]);
 }
 
 int main() {
@@ -15,6 +18,21 @@ int main() {
 	
 	while(t--) {
 		memset(checked,false,sizeof(checked));
+		memset(a,0,sizeof(a));
+		int n;
+		cin >> n;
 		
+		for(int  i = 1 ; i <= n ; i++) {
+			cin >> a[i];
+		}
+		
+		int cnt = 0;
+		for(int i = 1 ; i <= n ; i++) {
+			if(checked[i] == false) {
+				dfs(i);
+				cnt++;
+			}
+		}
+		cout << cnt << '\n';
 	}
 }
